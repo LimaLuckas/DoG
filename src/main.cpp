73850f62a2edcb5d6aa66e323064a812f1327471
LimaLuckas <lfreire05@gmail.com>
2025-03-27 00:51:25 -0300

    Addition of images and filtered examples

--- src/main.cpp
+++ src/main.cpp
@@ -50,15 +50,16 @@ void onPhiChange(int pos, void* data) {
 
 int main(){
 
-    std::string path = "../examples/mortal.jpg";
+    std::string path = "../examples/Stanczyk.jpg";
     Mat input = cv::imread(path);
     if (input.empty())
     {
         std::cerr << "Não foi possivel carregar a imagem, tente outro endereco" << std::endl;
         return -1;
     }
-    
-    DogFilter filter(input, true);
+
+    // true for colored blending with the filter, false for grayscale application of the filter
+    DogFilter filter(input, false);
 
     namedWindow("DoG Filter", WINDOW_AUTOSIZE);
 
@@ -66,7 +67,7 @@ int main(){
     createTrackbar("K", "DoG Filter", nullptr, 5000, onKChange, &filter);
     createTrackbar("P", "DoG Filter", nullptr, 5000, onPChange, &filter);
     createTrackbar("Phi", "DoG Filter", nullptr, 5000, onPhiChange, &filter);
-    createTrackbar("Epsilon", "DoG Filter", nullptr, 5000, onEpsilonChange, &filter);
+    createTrackbar("Epsilon", "DoG Filter", nullptr, 50000, onEpsilonChange, &filter);
 
     setTrackbarPos("Sigma", "DoG Filter", 1);
     setTrackbarPos("K", "DoG Filter", 1);
@@ -78,6 +79,7 @@ int main(){
     filter.applyFilter(output);
     imshow("DoG Filter", output);
 
+    //press any button on the keyboard to quit
     waitKey(0);
 
     return 0;
